97d48255fa053a568b70395942725496ef6055a0
Rodrigo Georgetti <rodrigogeorgetti.cba@gmail.com>
2024-03-22 12:45:42 -0300

    fix de social media

--- src/common/widgets/Socials/config/constans.tsx
+++ src/common/widgets/Socials/config/constans.tsx
@@ -1,4 +1,4 @@
-import { FiGithub, FiLinkedin, FiTwitter, FiInstagram, FiFacebook } from 'react-icons/fi'
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
 import { SocialTypes } from './types'
 
 
@@ -6,7 +6,4 @@ import { SocialTypes } from './types'
 export const socialData : SocialTypes[] = [
     { icon : FiGithub, url : "https://github.com/rodrigo-georgetti" },
     { icon : FiLinkedin, url : "https://www.linkedin.com/in/rodrigo-georgetti/" },
-    // { icon : FiTwitter, url : "https://twitter.com/raihanhosen01" },
-    // { icon : FiInstagram, url : "https://instagram.com/raihanhosen0001" },
-    // { icon : FiFacebook, url : "https://www.facebook.com/raihanhosen01" },
 ]
